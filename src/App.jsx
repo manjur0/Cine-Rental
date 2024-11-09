c21a208b764380094a0b8490445bd8cb76bd7767
manjur0 <sfmanjur21@gmail.com>
2024-11-09 17:04:27 +0600

    sidebar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Content from "./Components/Content";
 import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
 
 const App = () => {
   return (
     <div>
       <Header />
+
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <Content />
+        </div>
+      </main>
     </div>
   );
 };
